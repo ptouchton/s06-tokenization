690c828742cc16028f87b9ca175c105afd64d7c2
Paul Touchton <paultouchton@gmail.com>
2022-01-05 21:26:42 -0800

    working tokensale

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+import "./Crowdsale.sol";
+
+contract MyTokenSale is Crowdsale {
+
+    // KycContract kyc;
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+
+}
